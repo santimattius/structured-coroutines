c447f9936699023316d48b0880f99670d53dff02
Santiago Mattiauda <santimattius@gmail.com>
2026-02-01 16:47:07 -0300

    Add marketplace publishing and coroutine sample
    Configure IntelliJ plugin publishing: read the Marketplace token from a Gradle property or JB_MARKETPLACE_TOKEN env var and set the publishing channel to "default". Add a sample Kotlin file (BasicPluginErrorExample.kt) demonstrating incorrect coroutine patterns (passing Job/SupervisorJob into launch/withContext, creating CoroutineScope inline, and using GlobalScope) for use by tests or static analysis.

---
+++ sample/src/main/kotlin/io/github/santimattius/structured/sample/BasicPluginErrorExample.kt
@@ -0,0 +1,33 @@
+package io.github.santimattius.structured.sample
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+fun badJobInLaunch(scope: CoroutineScope) {
+    scope.launch(Job()) {           // ERROR!
+        println("Bad!")
+    }
+    scope.launch(SupervisorJob()) { // ERROR!
+        println("Also bad!")
+    }
+    withContext(SupervisorJob()) {  // ERROR!
+        println("Still bad!")
+    }
+}
+
+fun inlineScopeCreation() {
+    CoroutineScope(Dispatchers.IO).launch {
+        println("This is bad!")
+    }
+}
+
+fun globalScopeUsage() {
+    GlobalScope.launch {
+        println("This is also bad!")
+    }
+}
